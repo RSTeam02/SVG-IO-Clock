5a815810e172650c00b67efa6666e15da3c01e41
unknown <sakai.jun01@gmail.com>
2017-05-25 14:48:05 +0200

    parse json with each

--- msvc/binaryConverter.js
+++ msvc/binaryConverter.js
@@ -1,13 +1,12 @@
 class BinaryConverter {
 
-    
     convert(unit) {
         let binArr = [];
-        let timeUnit = [unit.hour, unit.min, unit.sec, unit.day, unit.month, unit.year];
 
-        for (let i = 0; i < timeUnit.length; i++) {
-            binArr[i] = ("000000" + parseInt(timeUnit[i]).toString(2)).slice(-7);
-        }
+        $.each(unit, function (k, v) {
+            binArr.push(("000000" + parseInt(v).toString(2)).slice(-7));
+        });
+
         return binArr;
     }
 }
