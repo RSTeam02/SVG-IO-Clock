71f0e6efa75dfe623469d717d4834db8bb21ed5f
Junichi <sakai.jun01@gmail.com>
2017-12-03 21:01:46 +0100

    font fix

--- SVGdom/svgTextObj.js
+++ SVGdom/svgTextObj.js
@@ -4,7 +4,7 @@ export class SVGTextObj {
         let txt = document.createElementNS("http://www.w3.org/2000/svg", "text");
         $(txt).attr("fill-opacity", .7);
         $(txt).attr("fill", "white");
-        $(txt).attr("font-family", "Geneva, Verdana, sans-serif");
+        $(txt).attr("font-family", "Geneva, sans-serif");
         $(txt).attr("font-size", "54px");
         txt.textContent = `${text.hour}:${text.min}:${text.sec} ${text.day}|${text.month}|${text.year}`;
         $("#nativeDisplay").append(txt);
